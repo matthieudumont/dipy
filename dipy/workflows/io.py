107424654e78518f6cfd7cc5c9bcbffaca9d85f0
Matthieu Dumont <matthieu.dumont@usherbrooke.ca>
2017-01-05 09:35:06 -0500

    Added first version of dicom convert

---
+++ dipy/workflows/io.py
@@ -0,0 +1,60 @@
+import re
+import inspect
+import logging
+import os
+import subprocess
+
+from dipy.workflows.workflow import Workflow
+
+
+def get_dicom_index(image_path, requested_tag):
+    command = 'mrinfo {0}'.format(image_path)
+    command_list = command.split(' ')
+
+    proc = subprocess.Popen(command_list, stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    _, stdout = proc.communicate(input='q')
+
+    dicom_idx = None
+    tags_regex = re.compile(r'(\d+)\s+-.*:\d\d (' + requested_tag + r') \(\*')
+    for idx, tag in tags_regex.findall(stdout):
+        if tag == requested_tag:
+            dicom_idx = idx
+            break
+
+    return dicom_idx
+
+
+def convert_dicom(dicom_path, dicom_index, out_path):
+    command = 'mrconvert {0} {1} -datatype uint16 {2}'.format(dicom_path, out_path, dicom_index)
+    command_list = command.split(' ')
+
+    subprocess.call(command_list)
+
+
+class ConvertDicomFlow(Workflow):
+    @classmethod
+    def get_short_name(cls):
+        return 'convert_dicom'
+
+    def run(self, input_files, tag, out_dir='',
+            out_file='dwi.nii.gz'):
+        """ Workflow for converting dicom files to nifti.
+
+        Parameters
+        ----------
+        input_files : string
+            Path to the input volumes. This path may contain wildcards to
+            process multiple inputs at once.
+        tag : string
+            Tag to find in the mrconvert output.
+        out_dir : string, optional
+            Output directory (default input file directory)
+        """
+
+        io_it = self.get_io_iterator()
+
+        for vol in io_it:
+            dicom_idx = get_dicom_index(vol, tag)
+            convert_dicom(vol, dicom_idx, out_file)
